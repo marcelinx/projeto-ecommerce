49bcced1576a54ee5fe29747078f2f67de56d3aa
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-01-12 20:29:13 -0300

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="hero">
+      <div class="card text-bg-dark text-white border-0">
+        <img src="./assets/bg.svg" class="card-img" alt="Background" height="550"/>
+        <div class="card-img-overlay d-flex flex-column justify-content-end">
+            <div className="container">
+            <h5 class="card-title display-3 fw-bolder mb-0"> ./Tech </h5>
+          <p class="card-text">
+          </p>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
